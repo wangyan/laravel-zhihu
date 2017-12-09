083e75873224160cfe2764eea888ac2f2470bb9c
WangYan <service@wangyan.org>
2017-12-10 00:40:08 +0800

    第十章 定义话题与问题关系

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['title','body','user_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany;
+     */
+    public function topics()
+    {
+        return $this->belongsToMany(Topic::class)->withTimestamps();
+    }
 }
