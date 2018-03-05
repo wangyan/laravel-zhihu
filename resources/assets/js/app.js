83bdefb97d7ed42e9ba5cf6107f9c405ee033f6b
WangYan <service@wangyan.org>
2018-03-06 00:38:06 +0800

    第二十章 使用 Vuejs 实现关注问题

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+/*Vue.component('example-component', require('./components/ExampleComponent.vue'));*/
+Vue.component('question-follow-button', require('./components/QuestionFollowButton.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -37,10 +37,11 @@
                     </div>
                     <div class="panel-body">
                         @if(Auth::check())
-                            <a href="/question/{{$question->id}}/follow"
+                            <!-- <a href="/question/{{$question->id}}/follow"
                                 class="btn btn-default {{Auth::user()->followed($question->id) ? 'btn-success' : ''}}">
                                 {{Auth::user()->followed($question->id) ? '已关注' : '关注该问题'}}
-                            </a>
+                            </a> -->
+                            <question-follow-button question="{{$question->id}}" user="{{Auth::id()}}"></question-follow-button>
                         @else
                             <a href="/question/{{$question->id}}/follow" class="btn btn-warning">关注该问题</a>
                         @endif
