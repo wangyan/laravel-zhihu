39bf99b6ac5a931b525c707a3b5a965170648293
WangYan <service@wangyan.org>
2018-06-02 22:25:45 +0800

    第四十四章 头像上传到七牛

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UsersController extends Controller
 {
@@ -14,9 +15,11 @@ class UsersController extends Controller
     public function changeAvatar(Request $request)
     {
         $file = $request->file('img');
-        $filename = md5(time().user()->id).'.'.$file->getClientOriginalExtension();
-        $file->move(public_path('avatars'),$filename);
-        user()->avatar = asset('avatars/'.$filename);
+        $filename = 'avatars/'.md5(time().user()->id).'.'.$file->getClientOriginalExtension();
+
+        Storage::disk('qiniu')->writeStream($filename,fopen($file->getRealPath(),'r'));
+        user()->avatar = 'http://'.config('filesystems.disks.qiniu.domain').'/'.$filename;
+
         user()->save();
         return ['url' => user()->avatar];
     }
